dd149ba96b2bd1c6a9c4995709e24bef3da97105
mozanfu-art <mozanfu@gmail.com>
2026-02-06 04:13:38 +0300

    update

--- import_db.php
+++ import_db.php
@@ -1,21 +1,18 @@
 <?php
-$servername = getenv('MYSQLHOST') ?: "mysql.railway.internal";
-$username = getenv('MYSQLUSER') ?: "root";
-$password = getenv('MYSQLPASSWORD') ?: "pWdzfiXDERePDBdxujdVHludnwhRvBPv";
-$dbname = getenv('MYSQLDATABASE') ?: "railway";
+$conn = new mysqli(
+    getenv('MYSQLHOST'),      // mysql.railway.internal
+    getenv('MYSQLUSER'),
+    getenv('MYSQLPASSWORD'),
+    getenv('MYSQLDATABASE'),
+    getenv('MYSQLPORT')
+);
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Read the SQL file
 $sql = file_get_contents('global_hotels_booking.sql');
 
-// Execute the SQL
 if ($conn->multi_query($sql)) {
     echo "Database imported successfully!";
 } else {
